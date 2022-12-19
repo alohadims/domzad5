a258e7cdd9e93b171d9c9636c799152b3da0ace4
79101 <sochnevd@list.ru>
2022-12-19 22:11:49 +0300

    homework5_complete

--- domzad5.py
+++ domzad5.py
@@ -2,8 +2,6 @@ ax = float(input('Введите координаты точки A по оси x
 ay = float(input('Введите координаты точки A по оси y:'))
 bx = float(input('Введите координаты точки B по оси x:'))
 by = float(input('Введите координаты точки B по оси y:'))
-
-
 import math
 rast = math.sqrt((ax-bx)**2+(ay-by)**2)
 print(f'Растояние между A и B = {rast}' )
